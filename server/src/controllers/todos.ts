eb71aa92b48a01784fd27f198f417797cd66c0fe
zhutongtong <zhutongtong@baidu.com>
2023-08-18 18:32:31 +0800

    添加server和client

---
+++ server/src/controllers/todos.ts
@@ -0,0 +1,35 @@
+import { Response, Request } from "express"
+import { ITodo } from "../types"
+import Todo from "../models/todo"
+
+const getTodos = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const todos: ITodo[] = await Todo.find()
+        res.status(200).json({ todos })
+    } catch (error) {
+        throw error
+    }
+}
+
+const addTodo = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const body = req.body
+        console.log(body, 11111)
+        console.log(req.body, 'req.body')
+        const todo: ITodo = new Todo({
+            name: body.name,
+            description: body.description,
+            status: body.status,
+        })
+
+        const newTodo: ITodo = await todo.save()
+        const allTodos: ITodo[] = await Todo.find()
+
+        res
+            .status(201)
+            .json({ message: "Todo added", todo: newTodo, todos: allTodos })
+    } catch (error) {
+        throw error
+    }
+}
+export { getTodos, addTodo }
